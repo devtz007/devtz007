64040a6f9e3c82715c5dee3fb93e6da217342b24
devtz007 <md.tazin@gmail.com>
2024-06-27 14:51:55 +0600

    u

--- update_readme.py
+++ update_readme.py
@@ -20,7 +20,6 @@ def fetch_wakatime_data():
 
 # Construct SVG content
 def construct_svg_content(data):
-    # Replace with your SVG construction logic
     svg_content = f"""
     <svg width="100" height="100" xmlns="http://www.w3.org/2000/svg">
         <foreignObject width="100" height="100">
