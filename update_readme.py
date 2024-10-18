f8621d2889c12800b4fae58b7e82223c4b9452e8
devtz007 <devtz007@proton.me>
2024-10-18 10:10:41 +0600

    u

--- update_readme.py
+++ update_readme.py
@@ -67,7 +67,7 @@ def update_readme_with_svg(svg_content):
   <svg width="100%" height="auto" xmlns="http://www.w3.org/2000/svg">
         <foreignObject width="100%" height="100%">
             <div xmlns="http://www.w3.org/1999/xhtml" style="font-family: Arial, sans-serif; font-size: 14px;">
-                <iframe src="https://tryhackme.com/api/v2/badges/public-profile?userPublicId=227999" style="border:none;"></iframe>
+                <img src="https://tryhackme.com/api/v2/badges/public-profile?userPublicId=227999" style="border:none;">
             </div>
         </foreignObject>
     </svg>
